acb6b73fa7d07406f2bbf17b8947d47e9baae096
Marc Verwoert <marc.verwoert@hotmail.com>
2017-05-01 16:08:56 +0200

    Create shop.py

---
+++ shop.py
@@ -0,0 +1,45 @@
+
+class FruitShop:
+    def __init__(self, name, fruitPrices):
+        """
+            name: Name of the fruit shop
+
+            fruitPrices: Dictionary with keys as fruit 
+            strings and prices for values e.g. 
+            {'apples':2.00, 'oranges': 1.50, 'pears': 1.75} 
+        """
+        self.fruitPrices = fruitPrices
+        self.name = name
+        print('Welcome to %s fruit shop' % name)
+
+    def getCostPerPound(self, fruit):
+        """
+            fruit: Fruit string
+        Returns cost of 'fruit', assuming 'fruit'
+        is in our inventory or None otherwise
+        """
+        if fruit not in self.fruitPrices:
+            return None
+        return self.fruitPrices[fruit]
+
+    def getPriceOfOrder(self, orderList):
+        """
+            orderList: List of (fruit, numPounds) tuples
+        Returns cost of orderList. If any of the fruit are
+        not in the shop, the function returns None.
+        """
+
+        " Insert your code for Problem 1 (buyLotsOfFruit.py). "
+
+        def buyLotsOfFruit(orderList):
+            price = 0
+            for fruit, pounds in orderList:
+                if fruit in self.fruitPrices:
+                    price += self.fruitPrices[fruit] * pounds
+                if fruit not in self.fruitPrices:
+                    return 'none'
+            return price
+        " Change fruitPrices to self.fruitPrices. "
+
+    def getName(self):
+        return self.name
